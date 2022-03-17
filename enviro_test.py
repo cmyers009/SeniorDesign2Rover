3a4ba784cdd853a7a2310d32dbe815d951be97b2
cmyers009 <89784819+cmyers009@users.noreply.github.com>
2022-03-16 21:36:46 -0400

    Update enviro_test.py

--- enviro_test.py
+++ enviro_test.py
@@ -1,29 +1,18 @@
+
 import time
 import board
-import digitalio
 from adafruit_bme280 import basic as adafruit_bme280
-import busio
-from adafruit_bus_device.spi_device import SPIDevice
-#import board
-#import busio
-#import digitalio
-#import adafruit_bmp280
-#spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-#cs = digitalio.DigitalInOut(board.D5)
-#sensor = adafruit_bmp280.Adafruit_BMP280_SPI(spi, cs)
 
-# Create sensfrom board import *or object, using the board's default I2C bus.
-#i2c = board.I2C()  # uses board.SCL and board.SDA
-#bme280 = adafruit_b me280.Adafruit_BME280_I2C(i2c)
-from board import *
-# OR create sensor object, using the board's default SPI bus.
 
-#spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+def run_environ():
+    # Create sensor object, using the board's default I2C bus.
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
-def run_bme():
-    spi = board.SPI()
-    bme_cs = digitalio.DigitalInOut(board.D26)
-    bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
+    # OR create sensor object, using the board's default SPI bus.
+    # spi = board.SPI()
+    # bme_cs = digitalio.DigitalInOut(board.D10)
+    # bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
 
     # change this to match the location's pressure (hPa) at sea level
     bme280.sea_level_pressure = 1013.25
